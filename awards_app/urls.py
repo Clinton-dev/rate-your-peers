fee392ebecf6164d204c9af16ef9ccdf11625b2e
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-12 00:58:42 +0300

    Add user settings view

--- awards_app/urls.py
+++ awards_app/urls.py
@@ -21,6 +21,7 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.registration, name='register'),
+    path('settings/', user_views.settings, name='settings'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
