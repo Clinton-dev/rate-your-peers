6a1a8531a0f5600e42279bbf6909cbb0d6899efe
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-11 23:34:18 +0300

    Add crispy forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def registration(request):
+    context = {
+        'title': 'signup',
+        'form': ''
+    }
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully, you can now login!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+        context['form'] = form
+
+    return render(request, 'users/register.html', context)
