2c3e7a6d2e38fe459384195ea43fd744f366b558
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-12 00:55:40 +0300

    Add logout functionality

--- awards_app/urls.py
+++ awards_app/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.registration, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
 ]
