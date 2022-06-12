ba984ac54be4b4443eabdb63ea2cdb00e039ba14
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-12 13:47:50 +0300

    Add apioverview view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def apiOverView(request):
+    api_urls = {
+        'Project List':'/project-list/',
+        'Detail View': '/project-detail/<str:pk>/',
+        'Create': '/project-create/',
+        'Update': '/project-update/<str:pk>/',
+        'Delete': '/project-delete/<str:pk>/',
+    }
+    return Response(api_urls)
