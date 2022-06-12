0a61305cc3cc24ed200060f4b41a4e5239380f84
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-12 14:09:55 +0300

    Add str method to project model

--- projects/models.py
+++ projects/models.py
@@ -10,4 +10,4 @@ class Project(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'self.title project'
+        return f'{self.title} project'
