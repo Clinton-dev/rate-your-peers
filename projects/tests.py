f06a48e078d0223563eedd96a78e195a455709e3
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-14 11:36:20 +0300

    Add test to user, rating and project model

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project, Rating
 
-# Create your tests here.
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='charles')
+        self.post = Project.objects.create(id=1, title='test post', description='short description',image='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e',
+                                        author=self.user, link='http://ur.coml')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Project))
+
+    def test_save_post(self):
+        self.post.save()
+        post = Project.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Project.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='charles')
+        self.post = Project.objects.create(id=1, title='test post', description='short description',image='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e',
+                                        author=self.user, link='http://ur.coml')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, project=self.post)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
+    def test_save_rating(self):
+        self.rating.save()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestUser(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='charles', password='wer2345uyq')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
